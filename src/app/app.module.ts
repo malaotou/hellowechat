8646105dd352325e8ce79e0e8327efc3340f3890
malaotou <marrator@163.com>
2019-06-03 12:08:08 +0800

    firestore”

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
@@ -12,12 +13,19 @@ import { CoreModule } from './core/core.module';
 import { UiModule } from './ui/ui/ui.module';
 
 const firebaseConfig = {
-  apiKey: "<Firebase API key>",
-  authDomain: "<Firebase auth domain>",
-  databaseURL: "<Firebase database URL>",
-  projectId: "<Firebase project ID>",
-  storageBucket: "<Firebase storage bucket>",
-  messagingSenderId: "<Firebase messaging sender ID>",
+  // apiKey: "<Firebase API key>",
+  // authDomain: "<Firebase auth domain>",
+  // databaseURL: "<Firebase database URL>",
+  // projectId: "<Firebase project ID>",
+  // storageBucket: "<Firebase storage bucket>",
+  // messagingSenderId: "<Firebase messaging sender ID>",
+  apiKey: "AIzaSyBAv0VLADqpJXf3LZnI5OmDeHsy-teWpBw",
+  authDomain: "hellowechat-b26dd.firebaseapp.com",
+  databaseURL: "https://hellowechat-b26dd.firebaseio.com",
+  projectId: "hellowechat-b26dd",
+  storageBucket: "",
+  messagingSenderId: "259430050677",
+  appId: "1:259430050677:web:7ba2323c861b4666"
 };
 @NgModule({
   declarations: [
@@ -28,6 +36,7 @@ const firebaseConfig = {
     AppRoutingModule,
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFirestoreModule,
+    AngularFireAuthModule,
     BrowserAnimationsModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     CoreModule,
--- src/app/ui/layout/layout.component.ts
+++ src/app/ui/layout/layout.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent implements OnInit {
-
-  constructor() { }
+  notes$: Observable<any[]>;
+  constructor(db: AngularFirestore) {
+    this.notes$ = db.collection('notes').valueChanges();
+  }
 
   ngOnInit() {
   }
